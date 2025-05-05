ccc6f5aa77dbb20029e4dd0b217d0f3a43785edc
Enzo_Ro_Haus <105392239+Enzo-Ro-Haus@users.noreply.github.com>
2025-05-05 14:53:34 +0000

    main

--- src/app.js
+++ src/app.js
@@ -9,4 +9,13 @@ const authorRoutes = require('./routes/author');
 const app = express();
 dotenv.config();
 
-app.use(cors({origin: ''}));
+app.use(cors({origin: 'http:/localhost:'}));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_DB)
+.then(() => {
+    console.log('Conectado a mongo db');
+    app.listen(process.env.PORT, () => {
+        console.log(`Servidor en el puerto ${process.env.PORT}`)
+    });
+}).catch((err) => console.error('Error en la conexión', err));
