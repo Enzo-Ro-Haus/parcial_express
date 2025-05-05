a20123542d1b179a4e8d43f7372b847778925299
Enzo_Ro_Haus <105392239+Enzo-Ro-Haus@users.noreply.github.com>
2025-05-05 14:48:46 +0000

    scrpt

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-const 
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const bookRoutes = require('./routes/book');
+const authorRoutes = require('./routes/author');
+
+const app = express();
+dotenv.config();
+
+app.use(cors({origin: ''}));
